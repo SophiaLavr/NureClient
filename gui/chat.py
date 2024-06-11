562722d03c4879af83aae28e23059ac69ee63290
SophiaLavr <149815626+SophiaLavr@users.noreply.github.com>
2024-06-11 14:06:18 +0300

    Update chat.py

--- gui/chat.py
+++ gui/chat.py
@@ -6,7 +6,7 @@ import textwrap
 import json
 
 OUTPUT_PATH = Path(__file__).parent
-ASSETS_PATH = OUTPUT_PATH / Path(r"C:\Users\Personal Computer\PycharmProjects\Client\gui\build\assets\frame0")
+ASSETS_PATH = OUTPUT_PATH / Path(r".\build\assets\frame0")
 
 def relative_to_assets(path: str) -> Path:
     return ASSETS_PATH / Path(path)
